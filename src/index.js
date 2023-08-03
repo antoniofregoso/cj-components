8feb782e9901de059d21e4d9eadcf3e707259e7a
antoniofregoso <yomero@antoniofregoso.com>
2023-08-02 18:05:32 -0600

    [ADD] Index

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { BlocksHtml } from "./BlocksHtml";
+import { BlocksImage } from "./BlocksImage";
+import { CallToAction } from "./CallToAction";
+import { CardList } from "./CardsList";
+import { CountdownTimer } from "./CountdownTimer";
+import { DiscountCoupon } from "./DiscountCoupon";
+import { FormLead } from "./FormLead";
+import { FunnelElement } from "./FunnelElement";
+import { FunnelFooter } from "./FunnelFooter";
+import { FunnelHeader } from "./FunnelHeader";
+import { FunnelPage } from "./FunnelPage";
+import { HeroBanner } from "./HeroBanner";
+import { HeroDouble} from "./HeroDouble";
+import { ImageText } from "./ImageText";
+import { LanguajeSelector } from "/LanguageSelector";
+import { LevelCentered } from "./LevelCentered";
+import { LinkinBio } from "./LinkinBio";
+import { SocialProof } from "./SocialProof";
+import { StepsComponent} from "./StepsComponent";
+import { SwiperElement } from "./SwiperElement";
+import { TakeMe } from "./TakeMe";
+import { VideoPlayer } from "./VideoPlayer";
+import { WebinarInvitation } from "./WebinarInvitation";
+import { WhatsappBubble } from "./WhatsappBubble";
+
+export { BlocksHtml, BlocksImage, CallToAction, CardList, CountdownTimer, DiscountCoupon, FormLead,
+    FunnelElement, FunnelFooter, FunnelHeader, FunnelPage, HeroBanner, HeroDouble, ImageText, LanguajeSelector,
+    LevelCentered, LinkinBio, SocialProof, StepsComponent, SwiperElement, TakeMe, VideoPlayer, WebinarInvitation,
+    WhatsappBubble }
